e433b66187ab5cfc3337e98e3eadc3127ca59727
s-vali <91510546+s-vali@users.noreply.github.com>
2023-10-24 22:51:12 -0400

    Add files via upload

--- Implementation/Read.java
+++ Implementation/Read.java
@@ -1,7 +1,37 @@
 
 public class Read {
 
+	//Attributes
+	private Temperature temperature;
+	private Sensor sensor;
 	
+	//Constructor
+	public Read(Sensor sensor, Temperature temperature) {
+		this.temperature = temperature;
+		this.sensor = sensor;
+	}
 	
+	//Getters and Setters
+	public Temperature getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(Temperature temperature) {
+		this.temperature = temperature;
+	}
+
+	public Sensor getSensor() {
+		return sensor;
+	}
+
+	public void setSensor(Sensor sensor) {
+		this.sensor = sensor;
+	}
+
+	//Methods
+	public Temperature readTemperatureHASHMAP(Location location) {
+		return null;
+	}
 	
-}
+
+} //end of class
