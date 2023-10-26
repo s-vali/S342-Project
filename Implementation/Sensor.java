eb044a7695c44c66b3608a6121d0a05130e17e81
s-vali <91510546+s-vali@users.noreply.github.com>
2023-10-25 21:08:51 -0400

    Add files via upload

--- Implementation/Sensor.java
+++ Implementation/Sensor.java
@@ -1,10 +1,12 @@
 
 public class Sensor {
 
+	//Attributes
 	private boolean isDeployed;
 	private int sensorID;
 	private Location location;
-	private Temperature temperature; //no Temperature temperature because the association class will hold the attributes Sensor and Temperature have in common
+	private Temperature temperature;
+	private SensorRegistry sensorRegistry; 
 	
 	//Constructor
 	public Sensor(boolean isDeployed, int sensorID) {
@@ -12,48 +14,21 @@ public class Sensor {
 		this.sensorID = sensorID;
 	}
 	
-	/*
 	public Sensor(boolean isDeployed, int sensorID, Location location, Temperature temperature) {
 		this.isDeployed = isDeployed;
 		this.sensorID = sensorID;
 		this.location = location;
 		this.temperature = temperature;
 	}
-	*/
 
 	//Getters and Setters methods
-	public boolean isDeployed() {
-		return isDeployed;
-	}
-
-	public void setDeployed(boolean isDeployed) {
-		this.isDeployed = isDeployed;
-	}
-
-	public int getSensorID() {
-		return sensorID;
-	}
-
-	public void setSensorID(int sensorID) {
-		this.sensorID = sensorID;
-	}
-
-	/*
-	public Location getLocation() {
-		return location;
-	}
-
-	public void setLocation(Location location) {
-		this.location = location;
-	}
-
-	public Temperature getTemperature() {
-		return temperature;
-	}
-
-	public void setTemperature(Temperature temperature) {
-		this.temperature = temperature;
-	}
-	*/
+	public boolean isDeployed() { return isDeployed; }
+	public void setDeployed(boolean isDeployed) { this.isDeployed = isDeployed; }
+	public int getSensorID() { return sensorID; }
+	public void setSensorID(int sensorID) { this.sensorID = sensorID; }
+	public Location getLocation() { return location; }
+	public void setLocation(Location location) { this.location = location; }
+	public Temperature getTemperature() { return temperature; }
+	public void setTemperature(Temperature temperature) { this.temperature = temperature; }
 	
 } //end of class
