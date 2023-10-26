ef07a77423395c4bc16d5f7ec68257552f89c1a4
Nicola-Bdewi <74018961+Nicola-Bdewi@users.noreply.github.com>
2023-10-26 12:35:33 -0400

    Maps class as singleton.

--- Implementation/Maps.java
+++ Implementation/Maps.java
@@ -9,27 +9,5 @@ public class Maps
     public static HashMap<Sensor, Location> getMaps() {
         return sensorLocationPairs;
     }
-
-
-//    public HashMap<Sensor, Location> getSensorLocationPairs() {
-//        return sensorLocationPairs;
-//    }
-//
-//    public void hasLocation(Location location)
-//    {
-//
-//    }
-//
-//    public void clearPairs (){
-//        sensorLocationPairs.clear();
-//    }
-//
-//
-//    @Override
-//    public String toString() {
-//        return "Maps{" +
-//                "sensorLocationPairs=" + sensorLocationPairs +
-//                '}';
-//    }
 }
 
